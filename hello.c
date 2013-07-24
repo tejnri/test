8e6a3d80ca5830656d77da5eaa4c8ee4d3653bd2
Tejaswi Sharma <tejaswi.sharma@meritnation.com>
2013-07-24 15:55:24 +0530

    commit

--- hello.c
+++ hello.c
@@ -5,5 +5,6 @@ int main(){
     printf("Its confusing");
     printf("Nothing is confusing");
     printf("Added a new line to check merge and fetch");
+    printf("Added a new line again to check merge and fetch");
     return 0;
 }
